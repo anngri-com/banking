1c49e268e302d5366d2fd72c5bd28d8eda4ef6ec
anngri-com <anna.gritsay@gmail.com>
2020-07-07 09:18:42 -0700

    added java project folder

---
+++ java-banking-project/Banking/src/com/annas/banking/User.java
@@ -0,0 +1,65 @@
+package com.annas.banking;
+
+public class User {
+	  private int userId; // primary key
+	  private String username; // not null, unique
+	  private String password; // not null
+	  private String firstName; // not null
+	  private String lastName; // not null
+	  private String email; // not null
+	  //private Role role;
+	  
+	  public User(String login, String password) {
+		  this.username = login;
+		  this.password = password;
+	  }
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	}
+
